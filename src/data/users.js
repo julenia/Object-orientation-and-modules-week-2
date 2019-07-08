26d83139bb2c3dffc0d9174d4e18cd798f6eb688
julenia <julia.jankowska@onet.eu>
2019-07-08 14:17:14 +0200

    step 4

--- src/data/users.js
+++ src/data/users.js
@@ -1,19 +1,31 @@
 module.exports.data = [
   {
     name: "Ava",
-    dateOfBirth: "1990-04-01"
+    dateOfBirth: "1990-04-01",
+    getBirthday: function() {
+      return Date.parse(this.dateOfBirth);
+    }
   },
   {
     name: "James",
-    dateOfBirth: "1968-02-06"
+    dateOfBirth: "1968-02-06",
+    getBirthday: function() {
+      return Date.parse(this.dateOfBirth);
+    }
   },
   {
     name: "Danielle",
-    dateOfBirth: "1987-09-15"
+    dateOfBirth: "1987-09-15",
+    getBirthday: function() {
+      return Date.parse(this.dateOfBirth);
+    }
   },
   {
     name: "Darnell",
-    dateOfBirth: "1982-06-22"
+    dateOfBirth: "1982-06-22",
+    getBirthday: function() {
+      return Date.parse(this.dateOfBirth);
+    }
   }
 ]
 
--- src/data/users2.js
+++ src/data/users2.js
@@ -1,19 +1,31 @@
 module.exports.data = [
   {
     name: "Ava",
-    birthday: 638928000000
+    birthday: 638928000000,
+    getBirthday: function() {
+      return this.birthday;
+    }
   },
   {
     name: "James",
-    birthday: -60048000000
+    birthday: -60048000000,
+    getBirthday: function() {
+      return this.birthday;
+    }
   },
   {
     name: "Danielle",
-    birthday: 558662400000
+    birthday: 558662400000,
+    getBirthday: function() {
+      return this.birthday;
+    }
   },
   {
     name: "Darnell",
-    birthday: 393552000000
+    birthday: 393552000000,
+    getBirthday: function() {
+      return this.birthday;
+    }
   }
 ]
 
