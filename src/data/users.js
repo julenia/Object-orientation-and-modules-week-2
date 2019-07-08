5254e0f41b4404ec5413237476b969b09b9f4734
julenia <julia.jankowska@onet.eu>
2019-07-08 15:19:17 +0200

    step 4

--- src/data/users.js
+++ src/data/users.js
@@ -1,26 +1,41 @@
-function getBirthday() {
-  return Date.parse(this.dateOfBirth)
+// function getBirthday() {
+//   return Date.parse(this.dateOfBirth)
+// }
+
+// module.exports.data = [
+//   {
+//     name: "Ava",
+//     dateOfBirth: "1990-04-01",
+//     getBirthday
+//   },
+//   {
+//     name: "James",
+//     dateOfBirth: "1968-02-06",
+//     getBirthday
+//   },
+//   {
+//     name: "Danielle",
+//     dateOfBirth: "1987-09-15",
+//     getBirthday
+//   },
+//   {
+//     name: "Darnell",
+//     dateOfBirth: "1982-06-22",
+//     getBirthday
+//   }
+// ]
+
+function User(name, dateOfBirth) {
+  this.name = name;
+  this.dateOfBirth = dateOfBirth;
+  this.getBirthday = function() {
+    return Date.parse(this.dateOfBirth);
+  }
 }
 
 module.exports.data = [
-  {
-    name: "Ava",
-    dateOfBirth: "1990-04-01",
-    getBirthday
-  },
-  {
-    name: "James",
-    dateOfBirth: "1968-02-06",
-    getBirthday
-  },
-  {
-    name: "Danielle",
-    dateOfBirth: "1987-09-15",
-    getBirthday
-  },
-  {
-    name: "Darnell",
-    dateOfBirth: "1982-06-22",
-    getBirthday
-  }
+  new User("Ava", "1990-04-01"),
+  new User("James", "1968-02-06"),
+  new User("Danielle", "1987-09-15"),
+  new User("Darnell", "1982-06-22")
 ]
